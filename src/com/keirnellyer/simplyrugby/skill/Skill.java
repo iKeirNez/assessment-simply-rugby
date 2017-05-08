cd743b79100f2a74dec1603dbabceb88e01948a7
Keir Nellyer <keir@nellyer.co.uk>
2017-05-08 14:09:58 +0100

    Fix various issues related to previous member super-class changes

--- src/com/keirnellyer/simplyrugby/skill/Skill.java
+++ src/com/keirnellyer/simplyrugby/skill/Skill.java
@@ -4,16 +4,16 @@ public class Skill {
     private final String name;
     private int value;
 
-    public Skill(String type) {
-        this(type, 1);
+    public Skill(String name) {
+        this(name, 1);
     }
 
-    public Skill(String type, int value) {
+    public Skill(String name, int value) {
         if (value < 1 || value > 5) {
             throw new IllegalArgumentException("Value must be between 1 and 5.");
         }
 
-        this.name = type;
+        this.name = name;
         this.value = value;
     }
 
--- web/member/skills.jsp
+++ web/member/skills.jsp
@@ -1,5 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<jsp:useBean id="user" scope="session" type="com.keirnellyer.simplyrugby.user.JuniorMember"/>
+<jsp:useBean id="user" scope="session" type="com.keirnellyer.simplyrugby.user.Member"/>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
