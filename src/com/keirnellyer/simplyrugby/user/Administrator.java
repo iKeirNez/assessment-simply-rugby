5054380c755e5a64f9bc941575163f5e78f87b73
Keir Nelyer <keir@nellyer.co.uk>
2017-05-23 16:20:02 +0100

    Add JavaDoc for new Administrator methods

--- src/com/keirnellyer/simplyrugby/user/Administrator.java
+++ src/com/keirnellyer/simplyrugby/user/Administrator.java
@@ -22,6 +22,14 @@ public class Administrator extends User {
         setNavigation(navigation);
     }
 
+    /**
+     * Updates the skill value for the provided {@link Member}, category and skill name.
+     *
+     * @param member the member
+     * @param category the name of the category
+     * @param skill the name of the skill
+     * @param value the value
+     */
     public void updateSkillValue(Member member, String category, String skill, int value) {
         member.getSkills().stream()
                 .filter(c -> c.getName().equalsIgnoreCase(category))
@@ -30,6 +38,13 @@ public class Administrator extends User {
                 .forEach(s -> s.setValue(value));
     }
 
+    /**
+     * Updates the comment for the provided {@link Member} and category name.
+     *
+     * @param member the member
+     * @param category the name of the category
+     * @param comment the comment
+     */
     public void updateCategoryComment(Member member, String category, String comment) {
         member.getSkills().stream()
                 .filter(c -> c.getName().equalsIgnoreCase(category))
